5e20207fa6e5d8b8335cd15ae17afed751109ae1
Denis Briceag <87911944+denisBriceag@users.noreply.github.com>
2022-04-20 20:15:49 +0300

    Update 4_task.ts

--- 4_task.ts
+++ 4_task.ts
@@ -47,31 +47,16 @@ interface Car {
   numberOfDoors: number;
 }
 
-// type ApiResponse<T> = {
-//   data: {
-//     [P in keyof T]: T[P];
-//   } & { pagination: number }; //Here we'll allow typescript to pass pagination inside data. We receive a union of types: All types of T + pagination:number
-
-//   errors: string[];
-// };
-
-// let a2: ApiResponse<User> = {
-//   data: { id: 1, name: "Denis", age: 23, pagination: 2 },
-//   errors: ["error"],
-// };
-
-//=====================================================================================================
-
 type ApiResponse<T> = {
   data: {
-    [key: string]: T | number; //Data will now hold the same types that T and number combines;
-    pagination: number; //Pagination which always be a number
-  };
-
+    [P in keyof T extends Car? "cars" : "users"] : T[]
+  
+  } & {pagination : number}
   errors: string[];
 };
 
-let a33: ApiResponse<User> = {
-  data: { someData: { id: 1, name: "Denis", age: 23 }, pagination: 2 },
-  errors: ["error"],
-}; //Here pagination always be apart of the data
+
+let a: ApiResponse2<Car> = {
+  data: { users : [{id: 1, color: "Denis", numberOfDoors: 23}, {id: 2, color: "Anastasia", numberOfDoors: 23}], pagination:2 }, 
+  errors:['err1', 'err2']
+};
